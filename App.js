2e7a675a13ee758963436e634f36276e5f8d4bc9
Jonny Porter <mailtojon@icloud.com>
2021-12-16 19:13:57 -0700

    Added basic UI inputs

--- App.js
+++ App.js
@@ -1,17 +1,74 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, TextInput, TouchableOpacity } from 'react-native';
+
 
 export default function App() {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>To sign up, enter a username and password.</Text>
       <StatusBar style="auto" />
+      <View style={styles.inputView}>
+  <TextInput
+    style={styles.TextInput}
+    placeholder="Username"
+    placeholderTextColor="#003f5c"
+    onChangeText={(email) => setEmail(email)}
+  />
+</View>
+ 
+<View style={styles.inputView}>
+  <TextInput
+    style={styles.TextInput}
+    placeholder="Password"
+    placeholderTextColor="#003f5c"
+    secureTextEntry={true}
+    onChangeText={(password) => setPassword(password)}
+  />
+</View>
+<TouchableOpacity>
+  <Text style={styles.forgot_button}>Forgot Password?</Text>
+</TouchableOpacity>
+<TouchableOpacity style={styles.loginBtn}>
+  <Text style={styles.loginText}>LOGIN</Text>
+</TouchableOpacity>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  inputView: {
+    backgroundColor: "#FFC0CB",
+    borderRadius: 30,
+    width: "70%",
+    height: 45,
+    marginBottom: 20,
+    alignItems: "center",
+  },
+  
+  TextInput: {
+    height: 50,
+    flex: 1,
+    padding: 10,
+    marginLeft: 20,
+  },
+  forgot_button: {
+    height: 30,
+    marginBottom: 30,
+  },
+  loginBtn:
+ {
+   width:"80%",
+   borderRadius:25,
+   height:50,
+   alignItems:"center",
+   justifyContent:"center",
+   marginTop:40,
+   backgroundColor:"#FF1493",
+ },
+
   container: {
     flex: 1,
     backgroundColor: '#fff',
